794fc75a8b91c5ddfd0da861e1926b0e464d82d6
singhpriansh <priyanshu.h786@gmail.com>
2021-10-15 09:41:52 +0530

    imorovement

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,10 @@ app.use(bodyParser.urlencoded({ extended : false }));
 app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "*"
+        );
     res.setHeader(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
