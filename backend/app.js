f612ce55644dc6a0c4fe1d16c2a60e79b0f6c3c3
singhpriansh <priyanshu.h786@gmail.com>
2021-11-01 23:00:11 +0530

    process.env.Mongo_atls_pss is to be replaced by password

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,7 @@ const postsRoutes = require("./routes/posts");
 const userauthRoutes = require("./routes/userauth");
 
 const app = express();
-mongoose.connect("mongodb+srv://priyanshu:spvmfufVafB7xxV@cluster0.c7q1k.mongodb.net/node-angular?retryWrites=true&w=majority")
-// mongoose.connect("mongodb+srv://priyanshu:"+process.env.Mongo_atls_pss+"@cluster0.c7q1k.mongodb.net/node-angular?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://priyanshu:"+process.env.Mongo_atls_pss+"@cluster0.c7q1k.mongodb.net/node-angular?retryWrites=true&w=majority")
     .then(() => {
         console.log('Connected to the database!')
     })
